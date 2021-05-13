ebe17ce07f1fd2f13a6bcbad606f47d599ffda17
Mike Sedillo <Mike.Sedillo@gmail.com>
2021-05-13 20:23:31 +0000

    Adding files needed for 2 VMs

--- var/vm/scripts/start-vm-2.sh
+++ var/vm/scripts/start-vm-2.sh
@@ -5,19 +5,19 @@
     -m 2048 \
     -smp 2 \
     -M pc \
-    -name Ubuntu-Guest \
+    -name Ubuntu-Guest-2 \
     -cdrom /var/vm/disk/ubuntu-20.04.2.0-desktop-amd64.iso \
-    -net nic,model=virtio,macaddr=00:DE:AD:BE:EF:F1 \
+    -net nic,model=virtio,macaddr=00:DE:AD:BE:EF:F2 \
     -net user \
     -display egl-headless \
     -k en-us \
-    -vnc :1 \
+    -vnc :2 \
     -machine kernel_irqchip=on \
     -global PIIX4_PM.disable_s3=1 \
     -global PIIX4_PM.disable_s4=1 \
     -cpu host \
     -usb -device usb-tablet \
-    -device vfio-pci,sysfsdev=/sys/bus/pci/devices/0000:00:02.0/f50aab10-7cc8-11e9-a94b-6b9d8245bfc1,rombar=0,display=on,x-igd-opregion=off
+    -device vfio-pci,sysfsdev=/sys/bus/pci/devices/0000:00:02.0/f50aab10-7cc8-11e9-a94b-6b9d8245bfc2,rombar=0,display=on,x-igd-opregion=off
 
 
 

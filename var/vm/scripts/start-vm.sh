0fd0599e1c06a771b9d89112855007b8db5921b6
Mike Sedillo <Mike.Sedillo@gmail.com>
2021-05-22 02:00:44 +0000

    chromeos vm

--- var/vm/scripts/start-vm.sh
+++ var/vm/scripts/start-vm.sh
@@ -2,21 +2,24 @@
 
 /usr/bin/qemu-system-x86_64 \
     -enable-kvm \
-    -m 2048 \
-    -smp 2 \
-    -M pc \
-    -name Ubuntu-Guest \
-    -cdrom /var/vm/disk/ubuntu-20.04.2.0-desktop-amd64.iso \
-    -net nic,model=virtio,macaddr=00:DE:AD:BE:EF:F1 \
-    -net user \
-    -display egl-headless \
+    -M q35 \
     -k en-us \
-    -vnc :1 \
-    -machine kernel_irqchip=on \
+    -smp cores=2 \
+    -m 2048 \
+    -name ChromeOS-VM1 \
+    -drive file=/var/vm/disk/vm1-chromeos.qcow2 \
     -vga none \
-    -global PIIX4_PM.disable_s3=1 \
-    -global PIIX4_PM.disable_s4=1 \
-    -cpu host \
-    -usb -device usb-tablet \
+    -nic user \
+    -no-hpet \
     -bios /usr/share/qemu/OVMF.fd \
-    -device vfio-pci,sysfsdev=/sys/bus/pci/devices/0000:00:02.0/f50aab10-7cc8-11e9-a94b-6b9d8245bfc1,rombar=0,display=on,x-igd-opregion=off
+    -vnc :1 \
+    -cpu host \
+    -device vfio-pci,sysfsdev=/sys/bus/pci/devices/0000:00:02.0/f50aab10-7cc8-11e9-a94b-6b9d8245bfc1,display=on,x-igd-opregion=on,xres=1900,yres=1080 \
+    -display egl-headless \
+    -machine q35,accel=kvm,kernel_irqchip=on,vmport=off \
+    -usb -device usb-tablet \
+    -global ICH9-LPC.disable_s3=1 \
+    -global ICH9-LPC.disable_s4=1 \
+    -device intel-iommu,caching-mode=on,device-iotlb=off \
+    -rtc base=localtime,clock=host
+

f4962e8cceda28291adb2eeba67b4948e0b02189
Mike Sedillo <Mike.Sedillo@gmail.com>
2021-05-22 02:05:50 +0000

    Revert "chromeos vm"
    This reverts commit 0fd0599e1c06a771b9d89112855007b8db5921b6.

--- var/vm/scripts/env.sh
+++ var/vm/scripts/env.sh
@@ -10,10 +10,8 @@ MASK=0x0000000000000402
 # BKM: To generate UUID, use uuid command.
 # For ease of idfentification, replicate and replace the last number with VGPU index
 VGPU1="f50aab10-7cc8-11e9-a94b-6b9d8245bfc1"
-#VGPU2="f50aab10-7cc8-11e9-a94b-6b9d8245bfc2"
-VGPU2=""
-#VGPU3="f50aab10-7cc8-11e9-a94b-6b9d8245bfc3"
-VGPU3=""
+VGPU2="f50aab10-7cc8-11e9-a94b-6b9d8245bfc2"
+VGPU3="f50aab10-7cc8-11e9-a94b-6b9d8245bfc3"
 VGPUS=" $VGPU1 $VGPU2 $VGPU3 "
 VGPU_TYPE="i915-GVTg_V5_4"
 BASEVGPU="/sys/bus/pci/devices/0000:00:02.0"
